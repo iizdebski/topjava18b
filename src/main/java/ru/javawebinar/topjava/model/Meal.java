5186900bab96706a5afba4d980079b139d3b00a2
SwAppCoder <iizdepskyy112@gmail.com>
2020-01-20 17:36:20 +0200

    1_1_HW0_stream

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Meal {
     private final LocalDateTime dateTime;
@@ -23,7 +25,9 @@ public class Meal {
         return description;
     }
 
-    public int getCalories() {
-        return calories;
-    }
+    public int getCalories() {return calories; }
+
+    public LocalDate getDate(){ return dateTime.toLocalDate();}
+
+    public LocalTime getTime() { return dateTime.toLocalTime(); }
 }
