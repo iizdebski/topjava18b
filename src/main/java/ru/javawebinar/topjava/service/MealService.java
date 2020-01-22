fecaf32d448599b6a6f24d4fa7d73f139ce25bc1
SwAppCoder <iizdepskyy112@gmail.com>
2020-01-22 22:34:31 +0200

    3_02_HW2_meal_layers

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,8 +1,35 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.lang.Nullable;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.DateTimeUtil;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
+
+    public MealService(MealRepository repository) { this.repository = repository; }
+
+    public Meal get(int id, int userId) { return checkNotFoundWithId(repository.get(id, userId), id); }
+
+    public void delete(int id, int userId) { checkNotFoundWithId(repository.delete(id, userId), id); }
+
+    public List<Meal> getBetweenDates(@Nullable LocalDate startDate, @Nullable LocalDate endDate, int userId) {
+        return repository.getBetween(
+                DateTimeUtil.createDateTime(startDate, LocalDate.MIN, LocalTime.MIN),
+                DateTimeUtil.createDateTime(endDate, LocalDate.MAX, LocalTime.MAX), userId);
+    }
+
+    public List<Meal> getAll(int userId) { return repository.getAll(userId); }
+
+    public void update(Meal meal, int userId) { checkNotFoundWithId(repository.save(meal, userId), meal.getId()); }
+
+    public Meal create(Meal meal, int userId) { return repository.save(meal, userId); }
 }
