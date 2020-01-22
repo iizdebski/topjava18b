8edef4ea8175b5eef45227edb657db89f9f28d1b
SwAppCoder <iizdepskyy112@gmail.com>
2020-01-22 11:33:45 +0200

    2_2_HW1_optional

---
+++ src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package ru.javawebinar.topjava.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) { super(message); }
+}
---
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -0,0 +1,12 @@
+package ru.javawebinar.topjava.web;
+
+import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
+
+public class SecurityUtil {
+
+    public static int authUserId() {
+        return 1;
+    }
+
+    public static int authUserCaloriesPerDay() { return DEFAULT_CALORIES_PER_DAY; }
+}
