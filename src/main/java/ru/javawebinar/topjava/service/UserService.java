af049c10c4c34e8a19cfdfa24c0191cfcc352fdc
SwAppCoder <iizdepskyy112@gmail.com>
2020-01-22 15:22:29 +0200

    2_6_annotation_processing

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
@@ -10,13 +12,11 @@ import java.util.List;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
+@Service
 public class UserService {
 
+    @Autowired
     private UserRepository repository;
-
-    public void setRepository(UserRepository repository) {
-        this.repository = repository;
-    }
 
     public User create(User user) {
         return repository.save(user);
--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserService;
 
@@ -10,12 +11,13 @@ import java.util.List;
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
-public class AbstractUserController {
+public abstract class AbstractUserController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
+    @Autowired
     private UserService service;
 
-    public List<User> getAll(){
+    public List<User> getAll() {
         log.info("getAll");
         return service.getAll();
     }
@@ -36,13 +38,13 @@ public class AbstractUserController {
         service.delete(id);
     }
 
-    public void update(User user, int id){
+    public void update(User user, int id) {
         log.info("update {} with id={}", user, id);
         assureIdConsistent(user, id);
         service.update(user);
     }
 
-    public User getByMail(String email){
+    public User getByMail(String email) {
         log.info("getByEmail {}", email);
         return service.getByEmail(email);
     }
