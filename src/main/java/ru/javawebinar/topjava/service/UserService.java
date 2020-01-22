5c15beaf29a1fd7ca0089ed0c7d32f817b30d55d
SwAppCoder <iizdepskyy112@gmail.com>
2020-01-22 17:27:50 +0200

    2_7_constructor_injection

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
@@ -15,8 +14,10 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 @Service
 public class UserService {
 
+    private final UserRepository repository;
+
     @Autowired
-    private UserRepository repository;
+    public UserService(UserRepository repository) { this.repository = repository; }
 
     public User create(User user) {
         return repository.save(user);
