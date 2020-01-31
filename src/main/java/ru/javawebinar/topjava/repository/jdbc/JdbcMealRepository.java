c1b8dcb0d03657e7eea1a94c170a2d98a2cb76ab
SwAppCoder <iizdepskyy112@gmail.com>
2020-01-31 12:22:22 +0200

    6 02 2 HW5 profile test

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -1,20 +1,19 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.dao.support.DataAccessUtils;
-        import org.springframework.jdbc.core.BeanPropertyRowMapper;
-        import org.springframework.jdbc.core.JdbcTemplate;
-        import org.springframework.jdbc.core.RowMapper;
-        import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-        import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-        import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-        import org.springframework.stereotype.Repository;
-        import ru.javawebinar.topjava.model.Meal;
-        import ru.javawebinar.topjava.repository.MealRepository;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-        import java.util.List;
+import java.util.List;
 
 @Repository
 public class JdbcMealRepository implements MealRepository {
@@ -80,11 +79,9 @@ public class JdbcMealRepository implements MealRepository {
     }
 
     @Override
-    public List<Meal> getBetweenInclusive(LocalDate startDate, LocalDate endDate, int userId) {
-        startDate = (startDate == null ? LocalDate.MIN : startDate);
-        endDate = (endDate == null ? LocalDate.MAX : endDate);
+    public List<Meal> getBetweenInclusive(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
         return jdbcTemplate.query(
                 "SELECT * FROM meals WHERE user_id=? AND CAST(date_time AS DATE) BETWEEN ? AND ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, startDate, endDate);
+                ROW_MAPPER, userId, startDateTime, endDateTime);
     }
 }
