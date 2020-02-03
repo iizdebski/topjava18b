b3f5b8531d8c3cf0e37de6185978a6796a43b062
SwAppCoder <iizdepskyy112@gmail.com>
2020-02-03 11:04:38 +0200

    6 10 _1

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Persistable;
 
 import javax.persistence.*;
 
+
 @MappedSuperclass
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
