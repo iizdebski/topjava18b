641e555005f0e4b2e27b5c6cfffdd58089ac88af
SwAppCoder <iizdepskyy112@gmail.com>
2020-03-01 14:04:41 +0200

    11 0 1 fix

--- src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -1,1 +1,24 @@
 
+package ru.javawebinar.topjava.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class ResourceControllerTest extends AbstractControllerTest {
+
+    ResourceControllerTest() {
+        super("/resources/css/style.css");
+    }
+
+    @Test
+    void resources() throws Exception {
+        perform(doGet())
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.valueOf("text/css")))
+                .andExpect(status().isOk());
+    }
+}
