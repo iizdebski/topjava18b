a4221af663b8b4c14da2105521277d575ee2ecf0
SwAppCoder <iizdepskyy112@gmail.com>
2020-03-02 18:07:03 +0200

    11 04 01

--- src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
@@ -1,33 +1,32 @@
 package ru.javawebinar.topjava.web;
 
-        import org.slf4j.Logger;
-        import org.slf4j.LoggerFactory;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.core.Ordered;
-        import org.springframework.core.annotation.Order;
-        import org.springframework.dao.DataIntegrityViolationException;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.http.converter.HttpMessageNotReadableException;
-        import org.springframework.validation.BindException;
-        import org.springframework.validation.BindingResult;
-        import org.springframework.web.bind.MethodArgumentNotValidException;
-        import org.springframework.web.bind.annotation.ExceptionHandler;
-        import org.springframework.web.bind.annotation.ResponseStatus;
-        import org.springframework.web.bind.annotation.RestController;
-        import org.springframework.web.bind.annotation.RestControllerAdvice;
-        import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-        import ru.javawebinar.topjava.util.ValidationUtil;
-        import ru.javawebinar.topjava.util.exception.ErrorInfo;
-        import ru.javawebinar.topjava.util.exception.ErrorType;
-        import ru.javawebinar.topjava.util.exception.IllegalRequestDataException;
-        import ru.javawebinar.topjava.util.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.util.exception.ErrorInfo;
+import ru.javawebinar.topjava.util.exception.ErrorType;
+import ru.javawebinar.topjava.util.exception.IllegalRequestDataException;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-        import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.Optional;
 
-        import java.util.Map;
-        import java.util.Optional;
-
-        import static ru.javawebinar.topjava.util.exception.ErrorType.*;
+import static ru.javawebinar.topjava.util.exception.ErrorType.*;
 
 @RestControllerAdvice(annotations = RestController.class)
 @Order(Ordered.HIGHEST_PRECEDENCE + 5)
@@ -60,7 +59,7 @@ public class ExceptionInfoHandler {
             Optional<Map.Entry<String, String>> entry = CONSTRAINS_I18N_MAP.entrySet().stream()
                     .filter(it -> lowerCaseMsg.contains(it.getKey()))
                     .findAny();
-            if(entry.isPresent()) {
+            if (entry.isPresent()) {
                 return logAndGetErrorInfo(req, e, false, VALIDATION_ERROR, messageUtil.getMessage(entry.get().getValue()));
             }
         }
