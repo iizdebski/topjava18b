02dff90a0603de1a9072d32a39f3e7c1b819f8d3
SwAppCoder <iizdepskyy112@gmail.com>
2020-03-04 12:59:01 +0200

    11 07 i18n

--- src/main/java/ru/javawebinar/topjava/web/GlobalExceptionHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/GlobalExceptionHandler.java
@@ -1,20 +1,25 @@
 package ru.javawebinar.topjava.web;
 
-        import org.slf4j.Logger;
-        import org.slf4j.LoggerFactory;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.web.bind.annotation.ControllerAdvice;
-        import org.springframework.web.bind.annotation.ExceptionHandler;
-        import org.springframework.web.servlet.ModelAndView;
-        import ru.javawebinar.topjava.AuthorizedUser;
-        import ru.javawebinar.topjava.util.ValidationUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.util.exception.ErrorType;
 
-        import javax.servlet.http.HttpServletRequest;
-        import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @Autowired
+    private MessageUtil messageUtil;
 
     @ExceptionHandler(Exception.class)
     public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
@@ -23,7 +28,9 @@ public class GlobalExceptionHandler {
 
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         ModelAndView mav = new ModelAndView("exception",
-                Map.of("exception", rootCause, "message", ValidationUtil.getMessage(rootCause), "status", httpStatus));
+                Map.of("exception", rootCause, "message", ValidationUtil.getMessage(rootCause),
+                        "typeMessage", messageUtil.getMessage(ErrorType.APP_ERROR.getErrorCode()),
+                        "status", httpStatus));
         mav.setStatus(httpStatus);
 
         // Interceptor is not invoked, put userTo
